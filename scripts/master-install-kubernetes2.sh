1ef7534e239781dc4eb1cb37803cb75b267b026f
s372039 <113588783+s372039@users.noreply.github.com>
2023-05-10 00:14:10 +0200

    Create master-install-kubernetes2.sh

---
+++ scripts/master-install-kubernetes2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+OS="xUbuntu_20.04"
+VERSION=1.26
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+sudo apt update
+sudo apt -y install cri-o cri-o-runc
+sudo sed -i 's/10.85.0.0/192.168.0.0/g' /etc/cni/net.d/100-crio-bridge.conflist
+sudo systemctl daemon-reload
+sudo systemctl restart crio
+sudo systemctl enable crio
+sudo systemctl enable kubelet
+sudo kubeadm config images pull
+sudo kubeadm init \
+  --pod-network-cidr=192.168.0.0/16 \
+  --cri-socket unix:///var/run/crio/crio.sock
